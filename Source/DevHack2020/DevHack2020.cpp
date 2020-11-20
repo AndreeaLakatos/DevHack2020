fee7258b17ae54ad9141a3ec8d49873517dffa1c
Andreea Lakatos <andreealakatos17@yahoo.com>
2020-11-20 12:40:45 +0000

    +Added project files

---
+++ Source/DevHack2020/DevHack2020.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DevHack2020.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DevHack2020, "DevHack2020" );
